d2f227989e3d7e5a71419d9afee40cf22a3b6056
3Emme <emmettbuentiempo@gmail.com>
2020-10-02 10:28:48 -0700

    pass Order test 1

--- OrderTrack/Models/Order.cs
+++ OrderTrack/Models/Order.cs
@@ -4,15 +4,16 @@ namespace VendApp.Models
 {
   public class Order
   {
-    public string Title { get; set; }
-    public int Id { get; }
+    public string OrderTitle { get; set; }
     private static List<Order> _instances = new List<Order> { };
-
     public Order(string title)
     {
-      Title = title;
+      OrderTitle = title;
       _instances.Add(this);
-      Id = _instances.Count;
+    }
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
 }
