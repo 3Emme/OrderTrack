ee85545a96d75c26d0bd68e76efd6541a397204b
3Emme <emmettbuentiempo@gmail.com>
2020-10-02 12:53:34 -0700

    write tests 2 and 3

--- OrderTrack.Tests/Models.Tests/VendorTests.cs
+++ OrderTrack.Tests/Models.Tests/VendorTests.cs
@@ -20,5 +20,31 @@ namespace VendApp.Tests
       Vendor newVendor = new Vendor("test");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    { 
+      // Arange
+      string vendorName = "Wolfman";
+      Vendor newVendor = new Vendor(vendorName);
+      // Act
+      string result = newVendor.VendorName;
+      // Assert
+      Assert.AreEqual(vendorName, result);
+    }
+    [TestMethod]
+    public void SetName_SetName_String()
+    {
+      // Arrange
+      string vendorName = "Wolfman";
+      Vendor newVendor = new Vendor(vendorName);
+
+      // Act
+      string updatedVendorName = "Count Chocula";
+      newVendor.VendorName = updatedVendorName;
+      string result = newVendor.VendorName;
+
+      // Assert
+      Assert.AreEqual(updatedVendorName, result);
+    }
   }
 }
