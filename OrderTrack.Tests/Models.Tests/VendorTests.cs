e88d88dad4829b4816ee02d935e946e9db7e3a64
3Emme <emmettbuentiempo@gmail.com>
2020-10-02 12:45:54 -0700

    write test 1

---
+++ OrderTrack.Tests/Models.Tests/VendorTests.cs
@@ -0,0 +1,24 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using VendApp.Models;
+
+namespace VendApp.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
