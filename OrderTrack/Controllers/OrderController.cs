e42dab977878ffdda0c52e88c11976d18cf8659d
3Emme <emmettbuentiempo@gmail.com>
2020-10-02 16:47:52 -0700

    add new route for orders to faciliate order form for vendors

---
+++ OrderTrack/Controllers/OrderController.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VendApp.Models;
+
+namespace VendApp.Controllers
+{
+  public class OrdersController : Controller
+  {
+    [HttpGet("/vendors/{vendorId}/orders/new")]
+    public ActionResult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+  }
+}
