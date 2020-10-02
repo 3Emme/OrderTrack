abcdda9040ae741588bd181395eebbecbf8d8655
3Emme <emmettbuentiempo@gmail.com>
2020-10-02 10:34:17 -0700

    write and pass test 2

--- OrderTrack.Tests/Models.Tests/OrderTests.cs
+++ OrderTrack.Tests/Models.Tests/OrderTests.cs
@@ -20,5 +20,15 @@ namespace VendApp.Tests
       Order newOrder = new Order("test");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+    [TestMethod]
+    public void GetTitle_ReturnsTitile_String()
+    { //Arange
+      string orderTitle = "Pastries for Werewolf bar mitzvah";
+      Order newOrder = new Order(orderTitle);
+      //Act
+      string result = newOrder.OrderTitle;
+      //Assert
+      Assert.AreEqual(orderTitle, result);
+    }
   }
 }
