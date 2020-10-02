87f48eef0700257c499f0735258b908a80de248b
3Emme <emmettbuentiempo@gmail.com>
2020-10-02 15:04:18 -0700

    write vendor controller intiial stucture

---
+++ OrderTrack/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using VendApp.Models;
+
+namespace VendApp.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+  }
+} 
